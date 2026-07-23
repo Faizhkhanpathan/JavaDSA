d1ea365292212dd07c991b8079f69c2b52be826d
Faiz Khan <pfaizkhan95@gmail.com>
2026-07-24 02:01:28 +0530

    new work added

---
+++ operator.java
@@ -0,0 +1,31 @@
+// class Main {
+//     public static void main(String[] args) {
+//         System.out.println((3>2) || (5>2));
+//     }
+// }
+// -------------------------------------------------------------------------
+// class Main {
+//     public static void main(String[] args) {
+//         System.out.println((3>2) && (5>2));
+//     }
+// }
+// ---------------------------------------------------------------------------
+// class Main {
+//     public static void main(String[] args) {
+//         System.out.println(!(3>2));
+//     }
+// }
+
+// --------------------------------------------------------------------------------
+
+// ✅ Arithmetic → + - * / % (Math)
+
+// ✅ Unary → ++ -- ! (One variable)
+
+// ✅ Relational → == != > < >= <= (Comparison)
+
+// ✅ Logical → && || ! (Combine conditions)
+
+// ✅ Assignment → = += -= *= /= %= (Assign/Update values)
+
+// ✅ Precedence Order → () → ++ → * / % → + - → Relational → Equality → && → || → =
